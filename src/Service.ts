9e0dd875fa3007ae1a8b8265c96b04150a438c05
Maximilian Alexander <mbalex99@gmail.com>
2017-07-21 11:36:33 -0700

    adding path to an endpoint

--- src/Service.ts
+++ src/Service.ts
@@ -1,7 +1,11 @@
 import { RealmObjectServer } from './Server'
 
 export default interface Service {
-
+    
+    /**
+     * an optional path to bind this service to an endpoint
+     */
+    path?: string
     /**
      * Called when the Server has called the `use` method 
      */
